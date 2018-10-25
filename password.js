6c4823e506106a76c227fb0a17728a361348abaf
rich@richnoah.com <rich@richnoah.com>
2018-10-24 23:22:30 -0500

    Updated files

--- password.js
+++ password.js
@@ -1,3 +1,3 @@
-let sampleWord = "astronaut";
+let sampleWord = "astronaut";//the test word
 let pwRegex = /(?=\w{5,})(?=.*\d{2,}.*)/; // This will check for 5+ characters and no repeating characters in a row
 let result = pwRegex.test(sampleWord);
