7e15e124efb6265b997ceb18106efabd3f287d8b
rich@richnoah.com <rich@richnoah.com>
2018-10-24 22:44:33 -0500

    Checking 5+ Characters and no repeats.

--- password.js
+++ password.js
@@ -1,3 +1,3 @@
 let sampleWord = "astronaut";
-let pwRegex = /(?=\w{5,})(?=.*\d{2,}.*)/; // Change this line
+let pwRegex = /(?=\w{5,})(?=.*\d{2,}.*)/; // This will check for 5+ characters and no repeating characters in a row
 let result = pwRegex.test(sampleWord);
